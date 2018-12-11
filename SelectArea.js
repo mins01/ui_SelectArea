093b98e4d2d511464c2fd6ace7ba089969317f54
공대여자 <mins01.lycos.co.kr@gmail.com>
2018-12-11 10:54:45 +0900

    toDraggable 를 따로 떼어냄

--- SelectArea.js
+++ SelectArea.js
@@ -6,6 +6,7 @@
 * @site : www.mins01.com
 * @date :2018-11-12~
 * @github : https://github.com/mins01/ui_SelectArea
+* @dependance : https://github.com/mins01/ui_toDraggable.git
 */
 
 // CustomEvent 호환용 (for IE)
@@ -26,95 +27,6 @@
 })();
 
 var SelectArea = (function(){
-  
-  /**
-  * toDraggable
-  * @param  {html_node} target
-  * @param  {function} cb_onpointerdown
-  * @param  {function} cb_onpointermove
-  * @param  {function} cb_onpointerup
-  * @return {object} {"enable":function , "disable":function}
-  */
-  var toDraggable = function(target,cb_onpointerdown,cb_onpointermove,cb_onpointerup){
-    var data={
-      "ing":false,
-      "x0":0,
-      "y0":0
-    }
-    var _getXY = function(evt){
-      var x = evt.clientX;
-      var y = evt.clientY;
-      if(evt.isPrimary ){
-        var x = evt.clientX;
-        var y = evt.clientY;
-      }else if(evt.touches && evt.touches[0]){
-        var touch = evt.touches[0];
-        var x = touch.X;
-        var y = touch.Y;
-      }else{
-        var x = evt.x;
-        var y = evt.y;
-      }
-      return [x,y];
-    }
-    var _onpointerdown = function(target,data,cb_onpointerdown){
-      // var target = evt.target;
-      return function(evt){
-        data.ing = true;
-        var xy = _getXY(evt);
-        data.x0 = xy[0];
-        data.y0 = xy[1];
-        if(cb_onpointerdown) cb_onpointerdown(evt,xy[0],xy[1]);
-        // evt.preventDefault();evt.stopPropagation(); //이벤트 중지 안시킴
-        document.addEventListener('pointermove',_onpointermove);
-        document.addEventListener('pointerup',_onpointerup);
-        return false;
-      }
-    }(target,data,cb_onpointerdown)
-    var _onpointermove = function(target,data,cb_onpointermove){
-      return function(evt){
-        if(!data.ing){return;}
-        var xy = _getXY(evt);
-        var gapX = xy[0]-data.x0;
-        var gapY = xy[1]-data.y0;
-        data.x0 = xy[0];
-        data.y0 = xy[1];
-        if(cb_onpointermove) cb_onpointermove(evt,gapX,gapY);
-        evt.preventDefault();evt.stopPropagation();
-        return false;
-      }
-    }(target,data,cb_onpointermove)
-    var _onpointerup = function(target,data,cb_onpointerup){
-      return function(evt){
-        if(data.ing && cb_onpointerup) cb_onpointerup(evt);
-        data.ing = false;
-        evt.preventDefault();evt.stopPropagation();
-        document.removeEventListener('pointermove',_onpointermove);
-        document.removeEventListener('pointerup',_onpointerup);
-        return false;
-      }
-    }(target,data,cb_onpointerup)
-    var _notouchmove = function(evt){ evt.preventDefault();evt.stopPropagation()	;return false;}
-    var _enable = false;
-    var enable = function(){
-      if(!_enable){
-        target.addEventListener('pointerdown',_onpointerdown);
-        target.addEventListener('touchmove',_notouchmove);  
-        _enable = true;
-      }
-      
-    }
-    var disable = function(){
-      if(_enable){
-        target.removeEventListener('pointerdown',_onpointerdown);
-        target.removeEventListener('touchmove',_notouchmove);
-        _enable = false;
-      }
-    }
-    enable();
-    
-    return {"enable":enable,"disable":disable}
-  }
   /**
   * UI 기본형 생성
   * @param  {html_node} target 기준 대상
